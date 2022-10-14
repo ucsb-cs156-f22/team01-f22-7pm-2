f8124200bb7ab66c06b915df5d2affd7d3107248
DylanC1222 <77405374+DylanC1222@users.noreply.github.com>
2022-10-13 21:21:42 -0700

    dc - created a controller test for university

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityControllerTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityControllerTests.java
@@ -9,23 +9,18 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import edu.ucsb.cs156.spring.backenddemo.services.CountryCodeQueryService;
 import edu.ucsb.cs156.spring.backenddemo.services.UniversityQueryService;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @WebMvcTest(value = UniversityController.class)
 public class UniversityControllerTests {
-  private ObjectMapper mapper = new ObjectMapper();
+
   @Autowired
   private MockMvc mockMvc;
   @MockBean
@@ -38,7 +33,7 @@ public class UniversityControllerTests {
     String fakeJsonResult="{ \"fake\" : \"result\" }";
     String name = "Santa Barbara";
     when(mockUniversityQueryService.getJSON(eq(name))).thenReturn(fakeJsonResult);
-
+    
     String url = String.format("/api/university/get?name=%s", name);
 
     MvcResult response = mockMvc
