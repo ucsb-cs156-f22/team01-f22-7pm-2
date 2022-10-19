7ee4b342f82aeb2fc2bd86d48d121631aee1f21e
DylanC1222 <77405374+DylanC1222@users.noreply.github.com>
2022-10-18 17:53:31 -0700

    dc - changed repo url to ours and team member names to team 7pm-2 member names

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeController.java
@@ -1,59 +1,60 @@
 package edu.ucsb.cs156.spring.backenddemo.controllers;
-
+ 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
+ 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-
+ 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.http.ResponseEntity;
-
+ 
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+ 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+ 
 @Api(description="Home Page with links to documentation")
 @Slf4j
 @RestController
 public class HomeController {
-    
-    @ApiOperation(value = "Get general info about the server, including link to api documentation")
-    @GetMapping("/")
-    public ResponseEntity<String> getHome() throws JsonProcessingException {
-        log.info("Home Page accessed");
-        ServletUriComponentsBuilder builder = ServletUriComponentsBuilder.fromCurrentRequestUri();
-        // builder.scheme("http");
-        URI uri = builder.build().toUri();
-
-        String body = getHomePageObjectJSON(uri.toString());
-        return ResponseEntity.ok().body(body);
-    }
-    
-    public static String getHomePageObjectJSON(String baseUrl) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-
-        Map<String, Object> resultMap = new HashMap<String, Object>();
-        resultMap.put("greeting","Greetings from Spring Boot!");
-
-        List<String> team = new ArrayList<String>();
-        team.add("Andrew L.");
-        team.add("Bryan T.");
-        team.add("Bryan Z.");
-        team.add("Kevin H.");
-        team.add("Seth V.");
-        team.add("Phill C.");
-        team.add("Pranav M.");
-        resultMap.put("team",team);
-        resultMap.put("repo","https://github.com/ucsb-cs156-f22/STARTER-team01");
-        resultMap.put("api-documentation", baseUrl + "swagger-ui/");
-        resultMap.put("actuator", baseUrl + "actuator/");
-        return mapper.writeValueAsString(resultMap);
-    }
+  
+   @ApiOperation(value = "Get general info about the server, including link to api documentation")
+   @GetMapping("/")
+   public ResponseEntity<String> getHome() throws JsonProcessingException {
+       log.info("Home Page accessed");
+       ServletUriComponentsBuilder builder = ServletUriComponentsBuilder.fromCurrentRequestUri();
+       // builder.scheme("http");
+       URI uri = builder.build().toUri();
+ 
+       String body = getHomePageObjectJSON(uri.toString());
+       return ResponseEntity.ok().body(body);
+   }
+  
+   public static String getHomePageObjectJSON(String baseUrl) throws JsonProcessingException {
+       ObjectMapper mapper = new ObjectMapper();
+ 
+       Map<String, Object> resultMap = new HashMap<String, Object>();
+       resultMap.put("greeting","Greetings from Spring Boot!");
+ 
+       List<String> team = new ArrayList<String>();
+       team.add("Matt N.");
+       team.add("Andy G.");
+       team.add("Hanson Y.");
+       team.add("Caleb L.");
+       team.add("Dylan C.");
+       team.add("Ethan E.");
+       resultMap.put("team",team);
+       resultMap.put("repo","https://github.com/ucsb-cs156-f22/team01-f22-7pm-2");
+       resultMap.put("api-documentation", baseUrl + "swagger-ui/");
+       resultMap.put("actuator", baseUrl + "actuator/");
+       return mapper.writeValueAsString(resultMap);
+   }
 }
+ 
+
